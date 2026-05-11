e26930e9e3e066106009fa65185e610e62b1e14a
kgrb21 <kagaribi0320@gmail.com>
2026-05-11 23:11:32 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
@@ -20,6 +20,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "quantum.h"
 
+#define OLED_TIMEOUT 120000
+
+#define TAP_CODE_DELAY 5
+
+#define POINTING_DEVICE_AUTO_MOUSE_ENABLE
+#define AUTO_MOUSE_DEFAULT_LAYER 2
+#define AUTO_MOUSE_TIME 500
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default (VIA)
@@ -70,12 +78,4 @@ void oledkit_render_info_user(void) {
 }
 #endif
 
-#define OLED_TIMEOUT 120000
 
-#define TAP_CODE_DELAY 5
-
-#define POINTING_DEVICE_AUTO_MOUSE_ENABLE
-#define AUTO_MOUSE_DEFAULT_LAYER 2
-#define AUTO_MOUSE_TIME 500
-
-#define DYNAMIC_KEYMAP_LAYER_COUNT 6
