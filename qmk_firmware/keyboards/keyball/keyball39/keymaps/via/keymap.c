f8592ed17cc264aae8c9cb141fa5deb7723f158b
kgrb21 <kagaribi0320@gmail.com>
2026-05-11 21:54:38 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
@@ -1,128 +1,129 @@
-/*
-Copyright 2022 @Yowkees
-Copyright 2022 MURAOKA Taro (aka KoRoN, @kaoriya)
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include QMK_KEYBOARD_H
+#include "quantum.h"
 
-// --- 追加: オートマウスレイヤー用の設定 ---
-#define MOUSE_LAYER_INDEX 2              // マウスレイヤーの番号（Remapの設定に合わせて変更してください）
+/* ==========================================================================
+   1. 独自キーコードの定義 (AML_TOなど)
+   ========================================================================== */
+enum custom_keycodes {
+  AML_TO = SAFE_RANGE, // オートマウス有効/無効切り替え
+  AML_I50,             // タイムアウト時間を増やす
+  AML_D50,             // タイムアウト時間を減らす
+  SCRL_DVI,            // スクロール速度調整
+  SCRL_DVD,
+  CPI_D1K,             // CPI調整
+  CPI_D100,
+  CPI_I100,
+  CPI_I1K,
+  KBC_SAVE,            // 設定保存
+  KBC_RST              // 設定リセット
+};
+
+/* ==========================================================================
+   2. オートマウスレイヤー(AML) の設定値
+   ========================================================================== */
+#define MOUSE_LAYER_INDEX 2              // マウスレイヤーの番号
 #define AUTO_MOUSE_LAYER_KEEP_TIME 30000 // 通常の維持時間（30秒）
 #define MOUSE_TIMEOUT_AFTER_CLICK 500    // クリック後の維持時間（0.5秒）
 #define AML_ACTIVATE_THRESHOLD 10        // 起動しきい値（誤爆防止）
 
+// 状態管理変数
 uint16_t aml_timer = 0;
 uint16_t aml_timeout = AUTO_MOUSE_LAYER_KEEP_TIME;
-bool aml_active = false;
+bool aml_active = false;      // 現在マウスレイヤーか
+bool aml_enable_sw = true;    // オートマウス機能自体のON/OFF
 static int16_t aml_x = 0;
 static int16_t aml_y = 0;
 
 #define ABS(x) ((x) < 0 ? -(x) : (x))
 
+/* ==========================================================================
+   3. キーマップ定義 (ご提示の universal レイアウトを完全維持)
+   ========================================================================== */
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [0] = LAYOUT_universal(
+    KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
+    KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_MINS  ,
+    KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  ,
+    KC_LCTL  , KC_LALT  , KC_LGUI  , KC_SPC   , MO(1)    , KC_BSPC  ,      _______  , MO(3)    , KC_ENT   , KC_TAB   , KC_RSFT  , KC_LSFT
+  ),
+
+  [1] = LAYOUT_universal(
+    KC_ESC   , KC_7     , KC_8     , KC_9     , KC_PLUS  ,                            KC_CIRC  , KC_AMPR  , KC_ASTR  , KC_LPRN  , KC_RPRN  ,
+    KC_LSFT  , KC_4     , KC_5     , KC_6     , KC_MINS  ,                            KC_HOME  , KC_UP    , KC_END   , KC_PGUP  , KC_BSLS  ,
+    KC_LCTL  , KC_1     , KC_2     , KC_3     , KC_ENT   ,                            KC_LEFT  , KC_DOWN  , KC_RGHT  , KC_PGDN  , KC_GRV   ,
+    _______  , _______  , KC_0     , KC_DOT   , _______  , KC_BSPC  ,      _______  , _______  , _______  , _______  , _______  , _______
+  ),
+
+  [2] = LAYOUT_universal(
+    KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                            KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   ,
+    KC_LSFT  , _______  , KC_BTN3  , _______  , KC_BSPC  ,                            S(KC_9)  , KC_BTN1  , KC_UP    , KC_BTN2  , KC_QUOT  ,
+    KC_SLSH  , KC_1     , KC_2     , KC_3     , S(KC_MINS),                           S(KC_NUHS), KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  ,
+    KC_ESC   , KC_0     , KC_DOT   , KC_DEL   , KC_ENT   , KC_BSPC  ,      _______  , _______  , _______  , _______  , _______  , _______
+  ),
+
+  [3] = LAYOUT_universal(
+    RGB_TOG  , AML_TO   , AML_I50  , AML_D50  , _______  ,                            _______  , _______  , _______  , _______  , _______  ,
+    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , SCRL_DVI ,                            _______  , _______  , _______  , _______  , _______  ,
+    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , SCRL_DVD ,                            CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , KBC_SAVE ,
+    QK_BOOT  , KBC_RST  , _______  , _______  , _______  , _______  ,      _______  , _______  , _______  , _______  , _______  , _______
+  )
+};
+
+/* ==========================================================================
+   4. ロジック実装部 (オートマウス機能)
+   ========================================================================== */
+
+// ボールの動きを検知してレイヤーを切り替える
 void report_mouse_user(report_mouse_t* mouse_report) {
+    if (!aml_enable_sw) return; // AML_TOキーでOFFにされている場合は無視
+
     if (mouse_report->x != 0 || mouse_report->y != 0) {
         if (!aml_active) {
-            // しきい値（THRESHOLD）の判定
+            // 移動量を蓄積
             aml_x += mouse_report->x;
             aml_y += mouse_report->y;
+            // しきい値を超えたときだけレイヤーON
             if (ABS(aml_x) > AML_ACTIVATE_THRESHOLD || ABS(aml_y) > AML_ACTIVATE_THRESHOLD) {
                 layer_on(MOUSE_LAYER_INDEX);
                 aml_active = true;
-                aml_x = 0;
-                aml_y = 0;
+                aml_x = 0; aml_y = 0;
             }
         }
         
         if (aml_active) {
-            // トラックボールを動かしている間はタイムアウトを30秒にリセット
+            // ボールを動かすと常にタイムアウトを30秒に延長
             aml_timeout = AUTO_MOUSE_LAYER_KEEP_TIME;
             aml_timer = timer_read();
         }
     }
 }
 
-// --- 追加: クリック後の離脱制御 ---
-    if (aml_active) {
-        if (keycode >= KC_MS_BTN1 && keycode <= KC_MS_BTN5) {
-            if (!record->event.pressed) {
-                // ボタンを離した瞬間、タイムアウトを短縮(0.5秒)に変更
+// キー操作の処理（AML_TOやクリック離脱の処理）
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case AML_TO: // 自動マウスレイヤーをトグル
+            if (record->event.pressed) {
+                aml_enable_sw = !aml_enable_sw;
+            }
+            return false;
+        case KC_MS_BTN1 ... KC_MS_BTN5: // マウスボタン
+            if (aml_active && !record->event.pressed) {
+                // ボタンを離した瞬間、タイムアウトを短い時間（0.5秒）に変更
                 aml_timeout = MOUSE_TIMEOUT_AFTER_CLICK;
                 aml_timer = timer_read();
             }
-        }
+            break;
     }
+    return true; 
+}
 
-// --- 追加: タイムアウト判定 ---
+// 常に動く監視処理（レイヤーの自動OFF）
+void matrix_scan_user(void) {
     if (aml_active) {
         if (timer_elapsed(aml_timer) > aml_timeout) {
             layer_off(MOUSE_LAYER_INDEX);
             aml_active = false;
-            aml_x = 0;
-            aml_y = 0;
+            aml_x = 0; aml_y = 0;
         }
     }
-
-#include "quantum.h"
-
-// clang-format off
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  // keymap for default (VIA)
-  [0] = LAYOUT_universal(
-    KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
-    KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_MINS  ,
-    KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  ,
-    KC_LCTL  , KC_LGUI  , KC_LALT  ,LSFT_T(KC_LNG2),LT(1,KC_SPC),LT(3,KC_LNG1),KC_BSPC,LT(2,KC_ENT),LSFT_T(KC_LNG2),KC_RALT,KC_RGUI, KC_RSFT
-  ),
-
-  [1] = LAYOUT_universal(
-    KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_RBRC  ,                            KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   ,
-    KC_F5    , KC_EXLM  , S(KC_6)  ,S(KC_INT3), S(KC_8)  ,                           S(KC_INT1), KC_BTN1  , KC_PGUP  , KC_BTN2  , KC_SCLN  ,
-    S(KC_EQL),S(KC_LBRC),S(KC_7)   , S(KC_2)  ,S(KC_RBRC),                            KC_LBRC  , KC_DLR   , KC_PGDN  , KC_BTN3  , KC_F11   ,
-    KC_INT1  , KC_EQL   , S(KC_3)  , _______  , _______  , _______  ,      TO(2)    , TO(0)    , _______  , KC_RALT  , KC_RGUI  , KC_F12
-  ),
-
-  [2] = LAYOUT_universal(
-    KC_TAB   , KC_7     , KC_8     , KC_9     , KC_MINS  ,                            KC_NUHS  , _______  , KC_BTN3  , _______  , KC_BSPC  ,
-   S(KC_QUOT), KC_4     , KC_5     , KC_6     ,S(KC_SCLN),                            S(KC_9)  , KC_BTN1  , KC_UP    , KC_BTN2  , KC_QUOT  ,
-    KC_SLSH  , KC_1     , KC_2     , KC_3     ,S(KC_MINS),                           S(KC_NUHS), KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  ,
-    KC_ESC   , KC_0     , KC_DOT   , KC_DEL   , KC_ENT   , KC_BSPC  ,      _______  , _______  , _______  , _______  , _______  , _______
-  ),
-
-  [3] = LAYOUT_universal(
-    RGB_TOG  , AML_TO   , AML_I50  , AML_D50  , _______  ,                            _______  , _______  , SSNP_HOR , SSNP_VRT , SSNP_FRE ,
-    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , SCRL_DVI ,                            _______  , _______  , _______  , _______  , _______  ,
-    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , SCRL_DVD ,                            CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , KBC_SAVE ,
-    QK_BOOT  , KBC_RST  , _______  , _______  , _______  , _______  ,      _______  , _______  , _______  , _______  , KBC_RST  , QK_BOOT
-  ),
-};
-// clang-format on
-
-layer_state_t layer_state_set_user(layer_state_t state) {
-    // Auto enable scroll mode when the highest layer is 3
-    keyball_set_scroll_mode(get_highest_layer(state) == 3);
-    return state;
 }
-
-#ifdef OLED_ENABLE
-
-#    include "lib/oledkit/oledkit.h"
-
-void oledkit_render_info_user(void) {
-    keyball_oled_render_keyinfo();
-    keyball_oled_render_ballinfo();
-    keyball_oled_render_layerinfo();
-}
-#endif
